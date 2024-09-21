c76540742750788f9610774a7d04ccd937eedcaf
092vk <vk092kumar@gmail.com>
2024-09-21 20:25:32 +0530

    added links to deployed site

--- frontend/app.js
+++ frontend/app.js
@@ -38,7 +38,7 @@ class Chatbox {
         this.updateChatText(chatbox);
 
         // Send the user's message to the server for prediction
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('https://healthchatbot-dr-shaboinky-1.onrender.com', {
             method: 'POST',
             body: JSON.stringify({ message: userMessage }),
             mode: 'cors',
